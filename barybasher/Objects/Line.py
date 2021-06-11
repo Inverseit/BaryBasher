2f16777619439dc5a0755750cb6daed6fcd4d3f0
seitkali_u <useital@andrew.cmu.edu>
2021-06-11 18:41:13 +0600

    line equality and intersection

--- barybasher/Objects/Line.py
+++ barybasher/Objects/Line.py
@@ -31,7 +31,7 @@ class Line():
             raise Exception(f"{p} is not a point")
         if not isinstance(q, Point):
             raise Exception(f"{q} is not a point")
-        return sympify(sympy.Matrix([[x, y, z], p.getAsList(), q.getAsList()]).det())
+        return sympy.Eq(sympify(sympy.Matrix([[x, y, z], p.getAsList(), q.getAsList()]).det()),0)
     
     def get_equation(self):
         return self.equation
@@ -48,6 +48,10 @@ class Line():
             return Point(0,0,0)
         s = list(s)
         px, py, pz = s[0]
+        variables = set()
+        variables = variables.union(px.free_symbols, py.free_symbols, pz.free_symbols)
+        if variables.intersection(set((x,y,z))):
+            raise Exception("Line you are comparing are the same line")
         return Point(px, py, pz)
     
     def __str__(self):
@@ -59,3 +63,20 @@ class Line():
         if self.equation != None:
             # print(list(self.equation))
             return res + f" -> Line defined with equation {self.equation}"
+    
+    def __eq__(self, o):
+        if not isinstance(o, Line):
+            raise Exception("o is not a line")
+        x, y, z = core.x, core.y, core.z
+        s = sympy.linsolve(
+            [self.equation, o.get_equation(), x+y+z-1], (x, y, z))
+        if not s:
+            return False
+        s = list(s)
+        px, py, pz = s[0]
+        variables = set()
+        variables = variables.union(
+            px.free_symbols, py.free_symbols, pz.free_symbols)
+        if variables.intersection(set((x, y, z))):
+            return True
+        return False
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from barybasher.Objects.Point import Point
 from barybasher.Objects.Line import Line
 from barybasher.Objects.Triangle import Triangle
+from barybasher.Objects.Circle import Circle
 import barybasher.core as core
 
 
@@ -11,20 +12,36 @@ import barybasher.core as core
 
 # # print(n)
 
+# I = ABC.getPoint("I")
+
+# ABI = Circle(points=(A, B, I))
+# CBI = Circle(points=(C, B, I))
+
+# r = ABI.radicalAxisWith(CBI)
+# IB =  Line(points=(I, B))
+# print(r == IB)
+
+# print(ABI.contains(A))
+
+
 ABC = Triangle(core.a, core.b, core.c)
 A, B, C = ABC.getVertices()
 K = ABC.getPoint("symmedian")
-n = ABC.getTangent("B")
-m = ABC.getTangent("C")
+# n = ABC.getTangent("B")
+# m = ABC.getTangent("C")
+n, m = Line(points=(A,B)), Line(points=(B,C))
+S = n.intersectWith(m)
+print(S)
 
-J = ABC.getPoint("I_A")
-I = ABC.getPoint("I")
+
+# def is_collinear(p, q, r):
+#     res = core.sympy.Matrix([r.getAsList(), p.getAsList(), q.getAsList()]).det().equals(core.sympy.S.Zero)
+#     if res:
+#         print(f"{p}, {q}, {r} are collinear")
+#     return res
+
+# print(is_collinear(K, S, A))
+
+# J = ABC.getPoint("I_A")
 
 
-def is_collinear(p, q, r):
-    res = core.sympy.Matrix([r.getAsList(), p.getAsList(), q.getAsList()]).det().equals(core.sympy.S.Zero)
-    if res:
-        print(f"{p}, {q}, {r} are collinear")
-    return res
-
-print(is_collinear(J, I, A))
